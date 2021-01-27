cc125dee51f65fbfd1e83ba91e8cd21be89275ed
immat0x1 <fl3ksmashina@gmail.com>
2021-01-27 18:51:40 +0500

    Customize.sh update

--- customize.sh
+++ customize.sh
@@ -1,26 +1,22 @@
-##########################################################################################
-#
-# MMT Extended Config Script
-#
-##########################################################################################
 
-##########################################################################################
-# Config Flags
-##########################################################################################
-# Enable flags by removing "#" from the start of the line
+# MMT-EX Config Flags
+
 # MINAPI=21
 # MAXAPI=25
 # DYNLIB=false
 # DEBUG=false
 
+# F-MMT Config Flags
+
 # With this flag you can choose which font will be used instead of Regular
 # Example: USE_AS_REGULAR=Bold
 USE_AS_REGULAR=Regular
 
-# With this flag you can enable automatical removing of all other font modules
+# With this flag you can enable automatical removing of all other font modules during installation
 # Change "false" to "true" to enable this flag
 DELETE_ANOTHER_FONT_MODULES=false
 
+# MMT-EX Logic
 SKIPUNZIP=1
 unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
 . $TMPDIR/functions.sh
