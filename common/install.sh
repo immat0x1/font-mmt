df4c35f8c635ad611eca7833996a6351f718232a
immat0x1 <fl3ksmashina@gmail.com>
2021-01-28 12:55:19 +0500

    Fixed many bugs (не осуждайте за быдлокод)

--- common/install.sh
+++ common/install.sh
@@ -7,6 +7,13 @@
 
 # Variables
 mpf=$MODPATH/Fonts
+xml=/system/etc/fonts.xml
+mxml=$(magisk --path)/.magisk/mirror/system/etc/fonts.xml
+sf=/system/fonts
+msf=$(magisk --path)/.magisk/mirror/system/fonts
+spf=/system/product/fonts
+mspf=$(magisk --path)/.magisk/mirror/system/product/fonts
+
 t=Thin l=Light
 r=Regular b=Bold
 it=Italic m=Medium
@@ -14,12 +21,21 @@ bl=Black c=Condensed
 s=Semibold exl=ExtraLight
 exb=ExtraBold
 
+[ -f $xml ] && mkdir -p $MODPATH$sf && mkdir -p $MODPATH/system/etc && cp -af $mxml $MODPATH$xml
+sed -i '/"sans-serif">/,/family>/H;1,/family>/{/family>/G}'	$MODPATH$xml
+sed -i ':a;N;$!ba;s/ name=\"sans-serif\"/ name="backup-roboto"/2' $MODPATH$xml
+sed -i '/\"backup-roboto\">/,/family>/{s/Roboto-/Backup-Roboto-/}' $MODPATH$xml
+sed -i 's/ name="backup-roboto"//g' $MODPATH$xml
+mkdir $MODPATH/Roboto
+find $msf -type f -name "Roboto*" | while read line; do
+cp -aR $line $MODPATH/Roboto; done
+
 # DELETE_ANOTHER_FONT_MODULES
 dafm() {
 find /data/adb/modules -path \*$1 | cut -d'/' -f-5 | while read line; do
-[ ! $line = $MODPATH ] && [ ! -f $line/system/fonts/NotoColorEmoji.ttf ] && rm -rf $line; ui_print " "; ui_print "* Found conflicting module!"; ui_print "* Deleting $line..."
+[ ! $line = "$MODPATH" ] && [ ! -f $line$sf/NotoColorEmoji.ttf ] && rm -rf $line; ui_print " "; ui_print "* Found conflicting module!"; ui_print "* Deleting $line..."
 line=${line//modules/modules_update}
-[ -d $line ] && [ ! $line = $MODPATH ] && [ ! -f $line/system/fonts/NotoColorEmoji.ttf ] && rm -rf $line; ui_print " "; ui_print "* Found conflicting module!"; ui_print "* Deleting $line..."; done
+[ -d $line ] && [ ! $line = "$MODPATH" ] && [ ! -f $line$sf/NotoColorEmoji.ttf ] && rm -rf $line; ui_print " "; ui_print "* Found conflicting module!"; ui_print "* Deleting $line..."; done
 }
 
 # All to ttf
@@ -32,28 +48,28 @@ for woff in $mpf/*.woff; do
 [ ! -f $mpf/$r.ttf ] && abort "* $r.ttf: not found"
 
 place_font() {
-find $1 -type f -name "*$2*" | while read line; do
-cp -ar $mpf/$2.ttf $MODPATH$line; done
+find $1 -type f -name "*$2*" | cut -d'/' -f6- | while read line; do
+cp -ar $mpf/$2.ttf $MODPATH/$line; done
 }
 
 replace_font() {
-find $1 -type f -name "*$2*" | while read line; do
-cp -ar $mpf/$3.ttf $MODPATH$line; done
+find $1 -type f -name "*$2*" | cut -d'/' -f6- | while read line; do
+cp -ar $mpf/$3.ttf $MODPATH/$line; done
 }
 
 main_func() {
-mkdir -p $MODPATH$1
+mkdir -p $MODPATH$2
 cd $1
 
 # USE_AS_REGULAR
 if [ ! $USE_AS_REGULAR = "$r" ] && [ -f "$mpf/$USE_AS_REGULAR.ttf" ]; then
-$2 "* You chose $USE_AS_REGULAR instead of $r"
+$3 "* You chose $USE_AS_REGULAR instead of $r"
 ls | while read line; do
-cp -ar $mpf/$USE_AS_REGULAR.ttf $MODPATH$1/$line; done
+cp -ar $mpf/$USE_AS_REGULAR.ttf $MODPATH$2/$line; done
 [ -f "$mpf/$USE_AS_REGULAR$it.ttf" ] && replace_font $1 $it $USE_AS_REGULAR$it
 else
 ls | while read line; do
-cp -ar $mpf/$r.ttf $MODPATH$1/$line; done; fi
+cp -ar $mpf/$r.ttf $MODPATH$2/$line; done; fi
 
 ### Non-Condensed ###
 for f in $it $b $b$it $m $m$it $bl $bl$it $t $t$it $l $l$it $s $s$it $exb $exb$it $exl $exl$it; do
@@ -64,14 +80,21 @@ for cf in $c-$r $c-$it $c-$b $c-$b$it $c-$m $c-$m$it $c-$l $c-$l$it; do
 [ -f $mpf/$cf.ttf ] && place_font $1 $cf; done
 
 # Clean-up unnecessary
-for uf in AndroidClock Noto Mono DancingScript DroidSans ComingSoon CarroisGothicSC; do
-find $MODPATH$1 -name "*$uf*.*" -exec rm -rf {} \;; done
-rm -rf $MODPATH/ExampleFontNames
+for uf in Noto Mono DancingScript DroidSans ComingSoon CarroisGothicSC; do
+find $MODPATH$2 -name "*$uf*.*" -exec rm -rf {} \;; done
 }
 
 # Start
 [ $DELETE_ANOTHER_FONT_MODULES = "true" ] && dafm "system/fonts"
-main_func "/system/fonts" "ui_print"
+main_func "$msf" "$sf" "ui_print"
 
 # Check if ROM have /system/product/fonts directory
-[ -d /system/product/fonts ] && main_func "/system/product/fonts"; [ $DELETE_ANOTHER_FONT_MODULES = "true" ] && dafm "system/product/fonts"
+[ -d $mspf ] && main_func "$mspf" "$spf"; [ $DELETE_ANOTHER_FONT_MODULES = "true" ] && dafm "system/product/fonts"
+
+# Restoring Roboto for fixing glyphs
+cd $MODPATH/Roboto
+find . -name "*Condensed*" -exec rm -rf {} \;
+rm -rf $MODPATH/ExampleFontNames
+for a in *; do mv "$a" $MODPATH$sf/Backup-"$a"; done
+rm -rf $MODPATH/Roboto
+rm -rf $MODPATH/Fonts
